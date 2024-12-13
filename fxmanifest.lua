453acf36db6690c649dc39a5c0b5017fb206ee7d
A ya Whitekali <dawju9@gmail.com>
2024-12-13 03:37:46 +0100

    new  files added to commit

---
+++ fxmanifest.lua
@@ -0,0 +1,37 @@
+fx_version 'bodacious'
+games {'gta5'}
+lua54 'yes'
+
+
+name 'Gang'
+description 'ESX Identity'
+author 'Erfan Ebrahimi'
+url 'http://erfanebrahimi.ir'
+
+version '0.0.3.4'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua'
+}
+
+client_scripts {
+	'shared/locale.lua',
+}
+
+export 'isOwnGangVehicle'
+
+server_exports {
+}
+
+
+server_scripts {
+	'BaseFunction.lua'
+}
+client_script 'BaseFunction.lua'
+
+dependencies {
+	'mysql-async',
+	'essentialmode',
+	'esplugin_mysql',
+	'async'
+}
